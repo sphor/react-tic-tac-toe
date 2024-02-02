85fcac5f5931574ff96510f735acc66c9cd88798
Joel Pratt <joeljpratt@gmail.com>
2024-02-02 13:51:56 -0600

    Initial commit, creates TicTacToe component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './components/TicTacToe';
+
+function App() {
+  return (
+    <TicTacToe />
+  );
+}
+
+export default App;
